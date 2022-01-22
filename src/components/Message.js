24a98fc18fe8eb8a5e5e8fe79f46c056d409197a
BhanuDeekshita-Anantha <bhanudeekshita599@gmail.com>
2022-01-22 11:39:05 +0530

    added time in message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,16 @@
 import React,{ Component } from 'react'
 
 class Message extends Component{
+    constructor(){
+        super();
+        this.state={
+            time:new Date().toLocaleString('en-US',{hour:'numeric',minute:'numeric',hour12:true,second:'numeric'})
+        }
+    }
     render(){
         return(
           <div>
-            <p>{this.props.msg}</p> 
+            <p>{this.props.msg}<span>{this.state.time}</span></p> 
           </div>
         )
 
